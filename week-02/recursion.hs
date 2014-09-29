82e461e9262969f8bd1d02ca72e8f0b1207413df
Lorand Szakacs <lsz@lorandszakacs.com>
2014-09-30 00:55:04 +0300

    Prettied up quicksort implementation

--- week-02/recursion.hs
+++ week-02/recursion.hs
@@ -1,6 +1,6 @@
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
-quicksort (head:tail) = smaller ++ pivot ++ larger
-                      where pivot = [head]
-                            smaller = filter (< head) (quicksort tail)
-                            larger = filter (> head) (quicksort tail)
+quicksort (head:tail) = smaller ++ [pivot] ++ larger
+                      where pivot = head
+                            smaller = filter (< pivot) (quicksort tail)
+                            larger = filter (> pivot) (quicksort tail)
